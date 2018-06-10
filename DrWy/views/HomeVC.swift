a828dc03a486a27c6279cadfc313fdc66b17e4dc
wangyu1997 <wangyu19970819@gmail.com>
2018-06-10 23:39:52 +0800

    上拉刷新和下拉加载

--- DrWy/views/HomeVC.swift
+++ DrWy/views/HomeVC.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Alamofire
 import AlamofireImage
+import GTMRefresh
 
 class HomeVC: UITableViewController {
     
@@ -16,14 +17,45 @@ class HomeVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadData()
         let nib = UINib.init(nibName: "HomeCell", bundle: nil)
         self.tableView.register(nib, forCellReuseIdentifier: "homeCell")        // Uncomment the following line to preserve selection between presentations
+        self.setUpTableView()
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    func setUpTableView(){
+        self.tableView.pullDownToRefreshText("亲，试试下拉会刷新的")
+            .releaseToRefreshText("亲，松开试试")
+            .refreshSuccessText("亲，成功了")
+            .refreshFailureText("亲，无果")
+            .refreshingText("亲，正在努力刷新")
+        self.tableView.gtm_addRefreshHeaderView {
+            [weak self] in
+            print("excute refreshBlock")
+            self?.refresh()
+        }
+        
+        self.tableView.gtm_addLoadMoreFooterView {
+            [weak self] in
+            print("excute loadMoreBlock")
+            self?.loadMore()
+        }
+    }
+    
+    
+    func refresh() {
+        refreshData()
+    }
+    
+    func loadMore() {
+        OperationQueue.main.addOperation {
+            self.tableView.endLoadMore(isNoMoreData: true)
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -42,25 +74,28 @@ class HomeVC: UITableViewController {
         return datas.count
     }
     
-    func loadData()  {
+    func refreshData()  {
         let url = news_api+"latest"
         Alamofire.request(url,method:.get, parameters: nil, encoding: JSONEncoding.default)
             .validate(contentType: ["application/json"])
             .responseJSON { response in
+                var load_flag = false
                 switch response.result {
                 case .success(let JSON):
-                    print("Success with JSON: \(JSON)")
                     let json = JSON as! NSDictionary
                     //example if there is an id
                    let storys = StoryList(fromDictionary: json)
-                    print(storys.date)
                     self.datas = storys.stories
+                    load_flag = true
                     OperationQueue.main.addOperation {
                         self.tableView.reloadData()
                         self.refreshControl?.endRefreshing()
                     }
                 case .failure(_):
-                    SwiftNotice.showNoticeWithText(.error, text: "获取数据失败", autoClear: true, autoClearTime: 3000)
+                    load_flag = false
+                }
+                OperationQueue.main.addOperation {
+                    self.tableView.endRefreshing(isSuccess: load_flag)
                 }
         }
     }
